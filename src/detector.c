a62f641c5e2884df8175da6ba12e16d96609acf0
zfcode <97576419@qq.com>
2019-11-30 14:03:38 +0800

    修改loss图像的最大值

--- src/detector.c
+++ src/detector.c
@@ -143,7 +143,7 @@ void train_detector(char *datacfg, char *cfgfile, char *weightfile, int *gpus, i
     args.threads = 3 * ngpus;   // Amazon EC2 Tesla V100: p3.2xlarge (8 logical cores) - p3.16xlarge
     //args.threads = 12 * ngpus;    // Ryzen 7 2700X (16 logical cores)
     mat_cv* img = NULL;
-    float max_img_loss = 5;
+    float max_img_loss = 20;
     int number_of_lines = 100;
     int img_size = 1000;
     img = draw_train_chart(max_img_loss, net.max_batches, number_of_lines, img_size, dont_show);
